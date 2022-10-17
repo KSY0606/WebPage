04cf7ee79f1334b1467a159fd820b7b44e6e4474
KSY0606 <tkddbs9506@naver.com>
2022-10-17 20:44:02 +0900

    header

--- reservation/js/header_scroll.js
+++ reservation/js/header_scroll.js
@@ -8,23 +8,25 @@ $(document).ready(function(){
       var scroll = $(window).scrollTop();
       //스크롤이 맨 위 일 경우
       if (scroll < 1) {
-        $("header").css("background-color" , "#FF3636");
+        $("header").css("background-color" , "#f94848");
+        $("header").css("border-bottom" , "");
 
         $(".top-menu > ul > li").css("color" , "#eaeaea");
 
         $(".logo > a").css("background-image" , 'url("' + logo + '")');
         $(".logo > a").css("background-size" , "130px 54px");
-        
+
         $(".search").css("background-image" , 'url("' + search + '")');
       }
+      
       // 스크롤이 내려갈 경우 변경
       else{
         $("header").css("background-color" , "#fff");
+        $("header").css("box-shadow" , "0 -4px 9px 1px #ccc");
 
         $(".top-menu > ul > li").css("color" , "#353535");
 
-        $(".logo > a").css("background-image" , 'url("' + sc_logo + '")');
-        $(".logo > a").css("background-size" , "130px 54px");
+        $(".logo > a").css("background-image" , 'url("' + logo + '")');
 
         $(".search").css("background-image" , 'url("' + sc_search + '")');
       }
