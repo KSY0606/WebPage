7259accaad22b480d4d9f88d8341389e0fea9b95
KSY0606 <tkddbs9506@naver.com>
2023-02-19 17:24:34 +0900

    숙소별 리스트 페이지

--- reservation/js/room_category/bed_type.js
+++ reservation/js/room_category/bed_type.js
@@ -5,5 +5,14 @@ function click_bed_type() {
     const twin = document.querySelector(".bed_type > div > p:nth-child(3) > span:nth-child(1) > img");
     const ondor = document.querySelector(".bed_type > div > p:nth-child(4) > span:nth-child(1) > img");
 
-    
+
+    if(single) {
+        single.style.top = "-21px";
+    }else if(double) {
+        double.style.top = "-21px";
+    }else if(twin) {
+        twin.style.top = "-21px";
+    }else {
+        ondor.style.top = "-21px";
+    }
 }
